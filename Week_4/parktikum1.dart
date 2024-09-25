bfe7212d0b8ed49d6d3e595dc0a3c454cb57ff17
Dhiyun <53220195+Dhiyun@users.noreply.github.com>
2024-09-26 03:29:59 +0700

    Delete Week_4 directory

--- Week_4/parktikum1.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  final List<String?> list = List.filled(5, null);
-    
-  list[1] = 'Dhika Wahyu Nugroho';
-  list[2] = '2241720056';
-
-  assert (list.length == 5);
-  print(list);
-}
